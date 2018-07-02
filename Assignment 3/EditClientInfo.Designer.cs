2bfcc2621c7bb278922e3a663ebfe99598ee032d
holloLaptop <ujkkju117@outlook.com>
2018-07-02 22:01:46 +0930

    View and Edit Client information, to be tested

---
+++ Assignment 3/EditClientInfo.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Assignment_3
+{
+    partial class EditClientInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_client = new System.Windows.Forms.Label();
+            this.getJobsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.agileDevelopmentDataSet = new Assignment_3.AgileDevelopmentDataSet();
+            this.getJobsTableAdapter = new Assignment_3.AgileDevelopmentDataSetTableAdapters.GetJobsTableAdapter();
+            this.comboBox_client = new System.Windows.Forms.ComboBox();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btn_updateClientInfo = new System.Windows.Forms.Button();
+            this.btn_editClientInfo = new System.Windows.Forms.Button();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_mobile = new System.Windows.Forms.TextBox();
+            this.txt_landLine = new System.Windows.Forms.TextBox();
+            this.txt_address = new System.Windows.Forms.TextBox();
+            this.txt_businessName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.queriesTableAdapter1 = new Assignment_3.AgileDevelopmentDataSetTableAdapters.QueriesTableAdapter();
+            this.getClientsTableAdapter1 = new Assignment_3.AgileDevelopmentDataSetTableAdapters.GetClientsTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_clientName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.getJobsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.agileDevelopmentDataSet)).BeginInit();
+            this.tabControl2.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_client
+            // 
+            this.lbl_client.AutoSize = true;
+            this.lbl_client.Location = new System.Drawing.Point(6, 12);
+            this.lbl_client.Name = "lbl_client";
+            this.lbl_client.Size = new System.Drawing.Size(84, 17);
+            this.lbl_client.TabIndex = 1;
+            this.lbl_client.Text = "Client Name";
+            // 
+            // getJobsBindingSource
+            // 
+            this.getJobsBindingSource.DataMember = "GetJobs";
+            this.getJobsBindingSource.DataSource = this.agileDevelopmentDataSet;
+            // 
+            // agileDevelopmentDataSet
+            // 
+            this.agileDevelopmentDataSet.DataSetName = "AgileDevelopmentDataSet";
+            this.agileDevelopmentDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // getJobsTableAdapter
+            // 
+            this.getJobsTableAdapter.ClearBeforeFill = true;
+            // 
+            // comboBox_client
+            // 
+            this.comboBox_client.DataSource = this.getJobsBindingSource;
+            this.comboBox_client.DisplayMember = "Name";
+            this.comboBox_client.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox_client.Enabled = false;
+            this.comboBox_client.FormattingEnabled = true;
+            this.comboBox_client.Location = new System.Drawing.Point(144, 12);
+            this.comboBox_client.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox_client.Name = "comboBox_client";
+            this.comboBox_client.Size = new System.Drawing.Size(258, 25);
+            this.comboBox_client.TabIndex = 7;
+            this.comboBox_client.ValueMember = "PersonID";
+            this.comboBox_client.SelectedIndexChanged += new System.EventHandler(this.comboBox_clent_SelectedIndexChanged);
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage2);
+            this.tabControl2.Location = new System.Drawing.Point(12, 12);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(430, 277);
+            this.tabControl2.TabIndex = 12;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btn_updateClientInfo);
+            this.tabPage2.Controls.Add(this.btn_editClientInfo);
+            this.tabPage2.Controls.Add(this.txt_email);
+            this.tabPage2.Controls.Add(this.txt_mobile);
+            this.tabPage2.Controls.Add(this.txt_landLine);
+            this.tabPage2.Controls.Add(this.txt_address);
+            this.tabPage2.Controls.Add(this.txt_clientName);
+            this.tabPage2.Controls.Add(this.txt_businessName);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.lbl_client);
+            this.tabPage2.Controls.Add(this.comboBox_client);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(422, 248);
+            this.tabPage2.TabIndex = 0;
+            this.tabPage2.Text = "Client Information";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btn_updateClientInfo
+            // 
+            this.btn_updateClientInfo.Location = new System.Drawing.Point(279, 212);
+            this.btn_updateClientInfo.Name = "btn_updateClientInfo";
+            this.btn_updateClientInfo.Size = new System.Drawing.Size(123, 23);
+            this.btn_updateClientInfo.TabIndex = 9;
+            this.btn_updateClientInfo.Text = "Update Info";
+            this.btn_updateClientInfo.UseVisualStyleBackColor = true;
+            this.btn_updateClientInfo.Click += new System.EventHandler(this.btn_updateClientInfo_Click_1);
+            // 
+            // btn_editClientInfo
+            // 
+            this.btn_editClientInfo.Location = new System.Drawing.Point(144, 213);
+            this.btn_editClientInfo.Name = "btn_editClientInfo";
+            this.btn_editClientInfo.Size = new System.Drawing.Size(123, 23);
+            this.btn_editClientInfo.TabIndex = 9;
+            this.btn_editClientInfo.Text = "Edit Client Info";
+            this.btn_editClientInfo.UseVisualStyleBackColor = true;
+            this.btn_editClientInfo.Click += new System.EventHandler(this.btn_editClientInfo_Click_1);
+            // 
+            // txt_email
+            // 
+            this.txt_email.Enabled = false;
+            this.txt_email.Location = new System.Drawing.Point(144, 184);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(258, 22);
+            this.txt_email.TabIndex = 8;
+            // 
+            // txt_mobile
+            // 
+            this.txt_mobile.Enabled = false;
+            this.txt_mobile.Location = new System.Drawing.Point(144, 156);
+            this.txt_mobile.Name = "txt_mobile";
+            this.txt_mobile.Size = new System.Drawing.Size(258, 22);
+            this.txt_mobile.TabIndex = 8;
+            this.txt_mobile.TextChanged += new System.EventHandler(this.txt_landLine_TextChanged);
+            // 
+            // txt_landLine
+            // 
+            this.txt_landLine.Enabled = false;
+            this.txt_landLine.Location = new System.Drawing.Point(144, 128);
+            this.txt_landLine.Name = "txt_landLine";
+            this.txt_landLine.Size = new System.Drawing.Size(258, 22);
+            this.txt_landLine.TabIndex = 8;
+            this.txt_landLine.TextChanged += new System.EventHandler(this.txt_landLine_TextChanged);
+            // 
+            // txt_address
+            // 
+            this.txt_address.Enabled = false;
+            this.txt_address.Location = new System.Drawing.Point(144, 99);
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(258, 22);
+            this.txt_address.TabIndex = 8;
+            // 
+            // txt_businessName
+            // 
+            this.txt_businessName.Enabled = false;
+            this.txt_businessName.Location = new System.Drawing.Point(144, 71);
+            this.txt_businessName.Name = "txt_businessName";
+            this.txt_businessName.Size = new System.Drawing.Size(258, 22);
+            this.txt_businessName.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 17);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Mobile";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "LandLine";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Business Name";
+            // 
+            // getClientsTableAdapter1
+            // 
+            this.getClientsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Client Name";
+            // 
+            // txt_clientName
+            // 
+            this.txt_clientName.Enabled = false;
+            this.txt_clientName.Location = new System.Drawing.Point(144, 43);
+            this.txt_clientName.Name = "txt_clientName";
+            this.txt_clientName.Size = new System.Drawing.Size(258, 22);
+            this.txt_clientName.TabIndex = 8;
+            // 
+            // EditClientInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 300);
+            this.Controls.Add(this.tabControl2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "EditClientInfo";
+            this.Text = "Edit Client";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.EmployeeJobsForm_FormClosed);
+            this.Load += new System.EventHandler(this.EmployeeJobs_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.getJobsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.agileDevelopmentDataSet)).EndInit();
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_client;
+        private AgileDevelopmentDataSet agileDevelopmentDataSet;
+        private System.Windows.Forms.BindingSource getJobsBindingSource;
+        private AgileDevelopmentDataSetTableAdapters.GetJobsTableAdapter getJobsTableAdapter;
+        private System.Windows.Forms.ComboBox comboBox_client;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_mobile;
+        private System.Windows.Forms.TextBox txt_landLine;
+        private System.Windows.Forms.TextBox txt_address;
+        private System.Windows.Forms.TextBox txt_businessName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btn_updateClientInfo;
+        private System.Windows.Forms.Button btn_editClientInfo;
+        private System.Windows.Forms.TextBox txt_clientName;
+        private System.Windows.Forms.Label label1;
+        private AgileDevelopmentDataSetTableAdapters.QueriesTableAdapter queriesTableAdapter1;
+        private AgileDevelopmentDataSetTableAdapters.GetClientsTableAdapter getClientsTableAdapter1;
+    }
+}
---
+++ Assignment 3/EditClientInfo.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Assignment_3
+{
+    // This form allows the contractor to access and edit imported jobs offline.
+    public partial class EditClientInfo : Form
+    {
+        //Package p = new Package();
+        DataTable Clients = new DataTable();
+        private OfflineMenuForm _offlineMenuForm; // Keeps track of the parent form of this form.
+
+        public EditClientInfo(OfflineMenuForm offlineMenuForm)
+        {
+            InitializeComponent();
+            _offlineMenuForm = offlineMenuForm;
+        }
+
+        // Check the input a user is entering.
+        private void txt_amountCharged_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow users to only enter numbers with a single decimal place; does NOT prevent incorrect 
+            //  input via Copy-Pasting.
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && 
+                (e.KeyChar != '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Show an additional set of data fields when the job is completed.
+        private void checkBox_completed_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void EmployeeJobs_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Format an imported file into the form to be edited.
+        private void btn_import_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void EmployeeJobsForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _offlineMenuForm.Show();
+        }
+
+        // Ensure only valid text is entered; automatically save the value when it is changed.
+        private void txt_amountCharged_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Update the form whenever a different job from the dropdown box is selected.
+        private void comboBox_jobs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lbl_client_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_description_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox_shortDescription_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Save the values related to a shift's start and end time whenever they are changed.
+        private void comboBox_shifts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_editClientInfo_Click(object sender, EventArgs e)
+        {
+            EnableClientFields(true);
+        }    
+
+        // Change the client's details according to the edits the contractor has made.
+        private void btn_updateClientInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Load the client's details.
+        private void LoadClientDetail()
+        {
+            this.getClientsTableAdapter1.Fill(this.agileDevelopmentDataSet.GetClients);
+
+            Clients = agileDevelopmentDataSet.GetClients;
+            foreach (DataRow row in Clients.Rows)
+            {
+                ComboboxItem CI = new ComboboxItem();
+                CI.Text = row[0].ToString() + ", " + row[2].ToString();
+                while (CI.Text.Contains("  ")) CI.Text = CI.Text.Replace("  ", " ");
+                CI.Value = row[1].ToString();
+                comboBox_client.Items.Add(CI);
+            }
+        }
+
+        // Change the Enabled value of the Client-related fields to allow editing (when needed).
+        private void EnableClientFields(bool value)
+        {
+            txt_clientName.Enabled = value;
+            txt_businessName.Enabled = value;
+            txt_address.Enabled = value;
+            txt_landLine.Enabled = value;
+            txt_mobile.Enabled = value;
+            txt_email.Enabled = value;
+        }
+
+        // Export the data via the Serialise function in Package.
+        private void btn_export_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Constantly check and save that valid text (numbers) is being entered.
+        private void txt_landLine_TextChanged(object sender, EventArgs e)
+        {
+            
+            TextBox Parent = (TextBox)sender;
+            int x = Parent.SelectionStart;
+
+            Parent.Text = Person.PhoneToString(Person.PhoneToInt(Parent.Text));
+            Parent.SelectionStart = x;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Automatically save the completion time when it is changed.
+        private void date_completedTime_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Automatically update the paid status when it is changed.
+        private void checkBox_paid_CheckStateChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Automatically update the notes of the contractor.
+        private void txt_notes_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox_clent_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = comboBox_client.SelectedIndex;
+            DataRow row = Clients.Rows[i];
+            
+
+            txt_businessName.Text = row[0].ToString();
+            txt_clientName.Text = row[2].ToString();
+            txt_email.Text = row[6].ToString();
+            txt_landLine.Text = row[4].ToString();
+            txt_mobile.Text = row[5].ToString();
+            txt_address.Text = row[3].ToString();
+            
+
+        }
+
+        private void btn_editClientInfo_Click_1(object sender, EventArgs e)
+        {
+            EnableClientFields(true);
+        }
+
+        private void btn_updateClientInfo_Click_1(object sender, EventArgs e)
+        {
+            
+            int id = int.Parse( ((ComboboxItem) comboBox_client.SelectedItem).Value.ToString());
+            queriesTableAdapter1.UpdateClientBusinessName(id, txt_businessName.Text);
+            queriesTableAdapter1.UpdatePersonName(id, txt_clientName.Text);
+            queriesTableAdapter1.UpdatePersonEmail(id, txt_email.Text);
+            queriesTableAdapter1.UpdatePersonLandLine(id, txt_landLine.Text);
+            queriesTableAdapter1.UpdatePersonMobile(id, txt_mobile.Text);
+            queriesTableAdapter1.UpdatePersonAddress(id, txt_address.Text);
+
+        }
+    }
+}
